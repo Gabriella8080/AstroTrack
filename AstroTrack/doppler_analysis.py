eb7ef281d4762a5ba6556a24f4df638d635c8f00
ada-f829be9315 <gmr24@ic.ac.uk>
2025-11-06 01:24:38 +0000

    flake8 edits

--- AstroTrack/doppler_analysis.py
+++ AstroTrack/doppler_analysis.py
@@ -1,7 +1,5 @@
-from datetime import datetime, time, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import pandas as pd
 from tabulate import tabulate
 
@@ -25,7 +23,7 @@ def plot_doppler_shifts(
          font_family (str): Font family for plot text.
          figsize (tuple): Figure size for each subplot.
          marker_color (str): Color of scatter plot markers.
-         time_window (float): Half-width of x-axis in around visibility center [min].
+         time_window (float): Half-width of x-axis around visibility [min].
 
     """
     c = 3.0e5  # speed of light in km/s
@@ -34,14 +32,18 @@ def plot_doppler_shifts(
         plt.figure(figsize=figsize)
 
         epochs = satellite_data["Epochs"]
-        velocity_r = np.array(satellite_data["Velocities"])  # relative velocity in km/s
+        velocity_r = np.array(
+            satellite_data["Velocities"]
+            )  # relative velocity in km/s
         elevations = satellite_data["Elevations"]
         tle_line_2 = satellite_data["TLE"][1]
         norad_id = tle_line_2.split()[1]
         theta_max = max(elevations)
         epochs_np = np.array([np.datetime64(e) for e in epochs])
         center_time = epochs_np[len(epochs_np) // 2]
-        relative_time = (epochs_np - center_time) / np.timedelta64(1, "m")  # minutes
+        relative_time = (
+            epochs_np - center_time) / np.timedelta64(1, "m"
+                                                      )  # minutes
         doppler_shift = (velocity_r / c) * f0
 
         plt.scatter(
@@ -53,11 +55,15 @@ def plot_doppler_shifts(
         )
 
         print_time = (
-            center_time.astype("datetime64[s]").tolist().strftime("%Y-%m-%d %H:%M:%S")
+            center_time.astype(
+                "datetime64[s]"
+                ).tolist().strftime("%Y-%m-%d %H:%M:%S")
         )
         plt.xlabel("Visibility Time (min)")
         plt.ylabel("Doppler Shift (kHz)")
-        plt.title(f"NORAD {norad_id} Doppler Shift (Centered at {print_time}):")
+        plt.title(
+            f"NORAD {norad_id} Doppler Shift (Centered at {print_time}):"
+            )
         plt.axhline(0, color="black", linestyle="--")
         plt.xlim(-time_window, time_window)
         plt.legend(loc="upper right", fontsize=10)
@@ -66,9 +72,14 @@ def plot_doppler_shifts(
 
 
 def check_doppler_resolution(
-    all_satellite_data, f0_array, resolution=12_000, experiment="REACH", return_df=False
+    all_satellite_data,
+    f0_array,
+    resolution=12_000,
+    experiment="REACH",
+    return_df=False
 ):
-    """Check if Doppler shift can be resolved, given experiment's frequency resolution.
+    """Check if Doppler shift can be resolved,
+    given experiment's frequency resolution.
 
     Parameters:
      all_satellite_data (list[dict]): Satellite data dictionaries.
@@ -90,7 +101,9 @@ def check_doppler_resolution(
             max_doppler_khz = np.max(np.abs((velocity_r / c) * f0)) / 1e3
             row[
                 f"{f0/1e6:.1f} MHz"
-            ] = f"{max_doppler_khz:.2f} kHz | {'YES' if max_doppler_khz > resolution/1e3 else 'NO'}"
+            ] = f"{max_doppler_khz:.2f} kHz | {
+                'YES' if max_doppler_khz > resolution/1e3 else 'NO'
+                }"
         row[f"Freq. to Resolve ({experiment})"] = f"{f_max_needed:.0f} MHz"
         rows.append(row)
 
